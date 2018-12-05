abaf26a40cca47464e9c41a0cdc4102f857580fb
PramodJana <Pramodjana395@gmail.com>
2018-12-05 21:29:08 +0530

    5th Dec

---
+++ aboutus.php
@@ -0,0 +1,523 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      <style>
+      
+      body
+          {
+              background-image: url(img1/about_body.jpeg);
+              /* Full height */
+    height: 100%; 
+
+    /* Center and scale the image nicely */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+          }
+      
+      
+      
+      
+      
+      
+      
+      
+      </style>
+
+    <title>inaudita | Contact Us</title>
+  </head>
+  <body>
+      <?php
+      include 'header.php';
+      ?>
+       <!-- -->
+     <div class="container-fluid" style="margin-top:115px;">
+  <center><h1 class="display-4"><b>It's all about us...</b></h1></center>
+  <center><p class="lead">Although the phrase is nonsense, it does have a long history. The phrase 
+  has been used for several centuries by typographers to show the most distinctive features of their fonts.
+  It is used because the letters involved and the letter spacing in those combinations reveal,
+  at their best, the weight, design, and other important features of the typeface.</p>
+
+
+<p class="lead">A 1994 issue of "Before & After" magazine traces 
+"Lorem ipsum ..." to a jumbled Latin version of a passage from de Finibus Bonorum et Malorum,
+ a treatise on the theory of ethics written by Cicero in 45 B.C. The passage "Lorem ipsum ..." is taken from text that reads, 
+"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit ...," which translates as,
+ "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</p>
+
+
+<p class="lead">During the 1500s, a printer adapted Cicero's text to develop a page of type samples.
+ Since then, the Latin-like text has been the printing industry's standard for fake, or dummy, text. 
+Before electronic publishing, graphic designers had to mock up layouts by drawing in squiggled lines to indicate text. 
+The advent of self-adhesive sheets preprinted with "Lorem ipsum" gave a more realistic way to indicate where text would go on a page.</p>
+  </center>
+</div>
+
+
+
+
+<div class="alert alert-success" role="alert">
+ <center> <h3>Founders</h3></center>
+</div>
+<div class="container">
+
+<div class="row">
+
+<div class="col-sm-4">
+<div class="card">
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+   <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+ Explore more
+</button>
+  </div>
+</div>
+</div>
+
+<div class="col-sm-4">
+<div class="card" >
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModa2">
+ Explore more
+</button>
+  </div>
+</div>
+</div>
+
+<div class="col-sm-4">
+<div class="card">
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModa3">
+ Explore more
+</button>
+  </div>
+</div>
+</div>
+
+</div>
+
+</div>
+
+
+
+
+<br>
+	
+	
+	
+	<div class="alert alert-danger" role="alert">
+ <center> <h3>Head</h3></center>
+</div>
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-sm-4">
+<div class="card">
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+   <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModa4">
+ Explore more
+</button>
+  </div>
+</div>
+</div>
+
+<div class="col-sm-4">
+<div class="card" >
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModa5">
+ Explore more
+</button>
+  </div>
+</div>
+</div>
+
+<div class="col-sm-4">
+<div class="card">
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModa6">
+ Explore more
+</button>
+  </div>
+</div>
+</div>
+
+</div>
+
+</div>
+
+
+<br>
+
+<div class="container" style="margin-right:100px">
+<div class="row">
+<div class="col">
+<div class="card" style="width: 25rem;">
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModa7">
+ Explore more
+</button>
+  </div>
+</div>
+</div>
+
+
+
+
+
+<div class="col">
+<div class="card" style="width: 25rem;">
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModa8">
+ Explore more
+</button>
+  </div>
+</div>
+</div>
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+</div>
+<br>
+
+
+<div class="alert alert-primary" role="alert">
+  <center><h1>Our Team</h1></center>
+</div>
+
+
+<div class="container-fluid">
+<div class="row">
+<div class="col">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+
+<div class="col">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+
+<div class="col">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+
+<div class="col">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+
+
+
+</div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="card">
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+     </div>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+<div class="modal fade" id="exampleModa2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabe2" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="card">
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title2</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+     </div>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+	
+	
+	
+	
+	<!-- Modal -->
+<div class="modal fade" id="exampleModa3" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabe3" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="card">
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title3</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+     </div>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+	
+	
+
+	<!-- Modal -->
+<div class="modal fade" id="exampleModa4" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabe4" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="card">
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title4</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+     </div>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+	
+
+<div class="modal fade" id="exampleModa5" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabe5" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="card">
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title5</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+     </div>
+  </div>
+</div>
+    </div>
+  </div>
+</div>	
+
+
+<div class="modal fade" id="exampleModa6" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabe6" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="card">
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title6</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+     </div>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+
+
+
+<div class="modal fade" id="exampleModa7" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabe7" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="card">
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title7</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+     </div>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+	
+	
+	
+	<div class="modal fade" id="exampleModa8" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabe8" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="card">
+  <img class="card-img-top" src="img/house.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title8</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+     </div>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+
+      
+      
+      <br>
+      <br>
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      <?php
+      include 'footer.php';
+      ?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,260 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="We Potrays Your Imaginaton">
+      <meta name="keywords" content="Inaudita, Inaudita,Let's The Emotion Speaks,We are the Storytellers,Lights, Camera, Action">
+      <meta name="author" content="Pramod Jana">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      <link href="style.css" rel="stylesheet">
+
+    
+      <!--Animate Css-->
+      <link rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+       <!--Wow JS-->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+
+
+
+
+
+    <title>Inaudita: We portray your imaginations</title>
+  </head>
+  <body>
+      <!--Wow JS Activation-->
+       <script>
+              new WOW().init();
+              </script>
+
+    <?php
+    include 'header.php';
+    ?>
+
+    <div id="carouselExampleFade" class="carousel slide carousel-fade sat1" data-ride="carousel" style="margin-top:91px;">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="img1/mainslidepic1.jpg" alt="First slide" style="height:666px">
+        <div class="carousel-caption d-none d-md-block">
+    <p class="tag wow fadeInUp">Let's The Emotion Speaks...</p>
+
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img1/mainslidepic2.jpg" alt="Second slide" style="height:666px">
+        <div class="carousel-caption d-none d-md-block">
+    <p class="tag">Let's The Emotion Speaks...</p>
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img1/mainslidepic3.jpg" alt="Third slide" style="height:666px">
+        <div class="carousel-caption d-none d-md-block">
+    <p class="tag">Let's The Emotion Speaks...</p>
+  </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+
+
+
+
+      <br>
+      <div class="container" >
+          <center class="wow fadeInDown"><h1 style="font-size: 63px">We are the Storytellers!</h1></center>
+          <hr>
+          <p class="wow fadeInUp">An all-rounder film production house in Delhi, Skittles Productions helps you narrate your story to the world in the most creative and comprehensible manner. Whether you are a brand, a start-up, an artist, or an individual, get a visionary and out of the box video solutions for all your marketing requirements. Our indestructible talent and relentless dedication make us one of the best film production houses in Delhi NCR.</p>
+      <p class="wow fadeInUP">Static Presentations are the days of past. If you want your next meeting or conference to be a memorable affair, grab the attention of your audience in the most dramatic and striking manner. Our services for corporate are cut out for such projects which we execute with much fervor and precision. Whether you need to pitch a new idea to the investors or tell people about your products and services in the most compelling manner, we are the leading video production company in India to deliver the most fitting videos for you.</p>
+      </div>
+      <br>
+      <br>
+
+
+
+      <div class="jumbotron" style="margin-bottom: 0rem;">
+          <div class="container">
+              <center><h1 style="font-size: 63px"  class="wow fadeInRight"><b>Lights! Camera! Action!</b></h1></center>
+          <hr>
+              <br>
+  <p class="lead wow fadeInLeft">Skittles Productions is making a name for itself in the league of top TV commercial production companies in India. Our quirky and noteworthy ideas are waiting for you bearing a golden opportunity. Adding creative films and documentaries to our strong suits, we also envision to become one of the finest video production company in India.</p>
+</div>
+          <br>
+
+
+
+          <div class="container">
+  <hr>
+              <br>
+  <div class="row">
+  <div class="col-sm-6">
+  <p class="lead wow fadeInLeft">
+  An all-rounder film production house in Delhi,
+  Skittles Productions helps you narrate your
+  story to the world in the most creative and comprehensible manner.
+  Whether you are a brand, a start-up, an artist, or an individual,
+  get a visionary and out of the box video
+  solutions for all your marketing requirements. Our indestructible talent
+  and relentless dedication make us one of the best film production houses in Delhi NCR.
+  </p>
+  </div>
+  <div class= "col-sm-6">
+    <div class="embed-responsive embed-responsive-16by9 video1">
+  <video width="400" controls>
+    <source src="Dil Zaffran Video Song _ Rahat Fateh Ali Khan _ Ravi Shankar _ Kamal Chandra _ Shivin _ Palak.mp4" type="video/mp4">
+    <source src="mov_bbb.ogg" type="video/ogg">
+    Your browser does not support HTML5 video.
+  </video>
+  </div>
+</div>
+</div>
+              <br>
+<hr>
+              <br>
+<div class="row">
+
+  <div class= "col-sm-6">
+    <div class="embed-responsive embed-responsive-16by9 video2">
+ <video width="400" controls>
+    <source src="Dil Zaffran Video Song _ Rahat Fateh Ali Khan _ Ravi Shankar _ Kamal Chandra _ Shivin _ Palak.mp4" type="video/mp4">
+    <source src="mov_bbb.ogg" type="video/ogg">
+    Your browser does not support HTML5 video.
+  </video>
+ </div>
+</div>
+
+<div class="col-sm-6">
+  <p class="lead wow fadeInRight">
+  An all-rounder film production house in Delhi,
+  Skittles Productions helps you narrate your
+  story to the world in the most creative and comprehensible manner.
+  Whether you are a brand, a start-up, an artist, or an individual,
+  get a visionary and out of the box video
+  solutions for all your marketing requirements. Our indestructible talent
+  and relentless dedication make us one of the best film production houses in Delhi NCR.
+  </p>
+  </div>
+</div>
+              <br>
+<hr>
+<div class="row">
+  <div class="col-sm-6">
+  <p class="lead wow fadeInLeft">
+  An all-rounder film production house in Delhi,
+  Skittles Productions helps you narrate your
+  story to the world in the most creative and comprehensible manner.
+  Whether you are a brand, a start-up, an artist, or an individual,
+  get a visionary and out of the box video
+  solutions for all your marketing requirements. Our indestructible talent
+  and relentless dedication make us one of the best film production houses in Delhi NCR.
+  </p>
+  </div>
+  <div class= col-sm-6>
+    <div class="embed-responsive embed-responsive-16by9 video3">
+  <video width="400" controls>
+    <source src="Dil Zaffran Video Song _ Rahat Fateh Ali Khan _ Ravi Shankar _ Kamal Chandra _ Shivin _ Palak.mp4" type="video/mp4">
+    <source src="mov_bbb.ogg" type="video/ogg">
+    Your browser does not support HTML5 video.
+  </video>
+</div>
+</div>
+</div>
+              <br>
+<hr>
+<div class="row">
+  <div class= col-sm-6>
+    <div class="embed-responsive embed-responsive-16by9 video4">
+  <video width="400" controls>
+    <source src="Dil Zaffran Video Song _ Rahat Fateh Ali Khan _ Ravi Shankar _ Kamal Chandra _ Shivin _ Palak.mp4" type="video/mp4">
+    <source src="mov_bbb.ogg" type="video/ogg">
+    Your browser does not support HTML5 video.
+  </video></div>
+</div>
+<div class="col-sm-6">
+  <p class="lead wow fadeInRight">
+  An all-rounder film production house in Delhi,
+  Skittles Productions helps you narrate your
+  story to the world in the most creative and comprehensible manner.
+  Whether you are a brand, a start-up, an artist, or an individual,
+  get a visionary and out of the box video
+  solutions for all your marketing requirements. Our indestructible talent
+  and relentless dedication make us one of the best film production houses in Delhi NCR.
+  </p>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          <br>
+      </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <?php
+    include 'footer.php';
+    ?>
+
+
+
+      <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
+</html>
